79cb7120be70eac022db58df6b378e82316e99bc
roberto <robypriz@gmail.com>
2019-04-11 21:36:00 +0200

    Read by jdbcTemplate

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,10 +1,13 @@
 package com.pluralsight.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -23,11 +26,18 @@ public class RideRepositoryImpl implements RideRepository {
 
 	@Override
 	public List<Ride> getRides() {
-		Ride ride = new Ride();
-		ride.setName("Corner Canyon");
-		ride.setDuration(120);
-		List <Ride> rides = new ArrayList<>();
-		rides.add(ride);
+
+		List<Ride> rides = jdbcTemplate.query("SELECT * FROM RIDE", new RowMapper<Ride>() {
+			@Override
+			public Ride mapRow(ResultSet resultSet, int i) throws SQLException {
+				Ride ride = new Ride();
+				ride.setId(resultSet.getInt("id"));
+				ride.setName(resultSet.getString("name"));
+				ride.setDuration(resultSet.getInt("duration"));
+				return ride;
+			}
+		});
+
 		return rides;
 	}
 	
